35b952461cf387c67c407c816f46aef79a29d461
Dawson Nelson <dawson.nelson123@gmail.com>
2018-11-12 12:10:45 -0700

    first commit

---
+++ server/server.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express')
+const massive = require('massive')
+
+
+const {
+    SERVER_PORT,
+    MASSIVE,
+} = process.env;
+
+
+
+const app = express();
+
+massive(MASSIVE).then(db => {
+    app.set('db', db);
+    console.log('db is connected');
+})
+
+
+app.use(express.json());
+ 
+
+app.listen(SERVER_PORT, () => console.log(`Listing on port ${SERVER_PORT}`))
